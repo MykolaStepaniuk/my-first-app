71f9608d8d733663ba5f9f724b04368c2d237f51
MYSK <mysk@simcorp.com>
2024-10-08 23:04:05 +0300

    Calc implementation

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -1,7 +1,12 @@
-﻿internal class Program
+﻿using MyFirstApp;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
+        Calc calc = new Calc();
+        calc.Add(10, 20);
+
         Console.WriteLine("Hello, Mykola!");
         Console.WriteLine("Hello");
         Console.WriteLine("Hi Branch1");

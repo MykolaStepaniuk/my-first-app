4174a023102a69c6a7a882fea87f6f2f2b08a264
MYSK <mysk@simcorp.com>
2024-10-09 09:19:37 +0300

    Feature branch 2 development

--- MyFirstApp/MyFirstApp/Program.cs
+++ MyFirstApp/MyFirstApp/Program.cs
@@ -5,5 +5,6 @@
         Console.WriteLine("Hello, Mykola!");
         Console.WriteLine("Hello");
         Console.WriteLine("Hi Branch1");
+        Console.WriteLine("Feature branch 2 - development 1");
     }
 }
